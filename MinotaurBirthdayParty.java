11fb15440ca4d0f98a72497fbe4135746ddb87f3
gns12 <garrettspears12@gmail.com>
2023-02-10 21:01:51 -0500

    Finished implementation of problem 1 with a lock to guarantee mutual exclusion whenever a guest/thread enters the maze

--- MinotaurBirthdayParty.java
+++ MinotaurBirthdayParty.java
@@ -1,6 +1,9 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MinotaurBirthdayParty {
     // Number of guests (threads) at the party 
-    private static final int NUM_GUESTS = 10;
+    public static final int NUM_GUESTS = 1000;
 
     public static boolean cupcakeAvailable = true;
     public static int numGuestsEatenCupcake = 0;
@@ -10,19 +13,47 @@ public class MinotaurBirthdayParty {
             Guest guest = new Guest(i);
             guest.start(); 
         }
+
+        while (numGuestsEatenCupcake < NUM_GUESTS) {
+            ;
+        }
+
+        System.out.println("All " + NUM_GUESTS + " have eaten a cupcake.");
     }
 }
 
 class Guest extends Thread {
-    // Unique identifier for each guest thread
-    private static int guestId;
+    private boolean eatenCupcake;
+    private Lock lock;
 
     public Guest(int guestId) {
-        this.guestId = guestId;
+        this.eatenCupcake = false;
+        this.lock = new ReentrantLock();
     }
 
     @Override
     public void run() {
-        System.out.println("Guest " + guestId + " ran");
+        while (MinotaurBirthdayParty.numGuestsEatenCupcake < MinotaurBirthdayParty.NUM_GUESTS) {
+            // Use lock since only one guest can enter the maze at a time
+            lock.lock();
+            try {
+                // If cupcake is not at the end of the maze, get servants to refill the cupcake immediately
+                if (!MinotaurBirthdayParty.cupcakeAvailable) {
+                    MinotaurBirthdayParty.cupcakeAvailable = true;
+                }
+                // This guest has not eaten a cupcake yet and the cupcake is available, so guest should eat the
+                // cupcake and increment the counter for the number of guests that have eaten a cupcake
+                else if (!eatenCupcake) {
+                    this.eatenCupcake = true;
+                    MinotaurBirthdayParty.numGuestsEatenCupcake++;
+                }
+                // If cupcake is available but guest has already eaten it, the current guest should leave
+                // the cupcake there so that other guests can have their chance to eat one
+            }
+            finally {
+                // Guest leaves the maze, so release the lock so that the Minotaur may choose other guests to enter
+                lock.unlock();
+            }
+        }
     }
 }
