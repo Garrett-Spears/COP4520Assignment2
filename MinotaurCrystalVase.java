0e3d46aa915afcaad8b6da9922a2b9e09bb0dead
gns12 <garrettspears12@gmail.com>
2023-02-23 23:04:09 -0500

    Made modifications to problem 2 to keep running until all guests have seen the vase.

--- MinotaurCrystalVase.java
+++ MinotaurCrystalVase.java
@@ -1,8 +1,8 @@
-import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MinotaurCrystalVase {
     // Number of guests (threads) at the party
@@ -11,8 +11,13 @@ public class MinotaurCrystalVase {
     // Boolean flag that decides whether or not each guest's visit to the vase should be printed out
     public static final boolean PRINT_VASE_VISITORS = true;
 
-    // Thread-safe queue that decides order of guests that will visit the vase one at a time
+    // Thread-safe queue that decides order of guests that will visit the vase one at a time. Capacity of this queue
+    // is equal to the total number of guests since there will never be more than the total number of guests at the party in this
+    // queue at any time.
     public static BlockingQueue<Guest> queueForVase = new ArrayBlockingQueue<>(NUM_GUESTS);
+
+    // Thread-safe counter that keeps track of how many guests have viewed the vase currently
+    public static AtomicInteger numGuestsVisitedVase = new AtomicInteger(0);
 
     public static void main(String[] args) {
         // Start all guest threads
@@ -23,9 +28,10 @@ public class MinotaurCrystalVase {
 
         long startTime = System.currentTimeMillis();
 
-        // Keep the showroom open for all guests to keep viewing while there are still guests at the party (main thread is not counted as a guest)
+        // Keep the showroom open for all guests to keep viewing while there are still guests at the party
         while (Thread.activeCount() > 1) {
-            // There are currently still guests at the party, but no one is currently in line, so keep the showroom open and continue operation
+            // There are currently still guests at the party who have not seen the vase, but no one is currently in line, 
+            // so keep the showroom open and continue operation
             if (queueForVase.isEmpty()) {
                 continue;
             }
@@ -34,14 +40,15 @@ public class MinotaurCrystalVase {
             // enters the showroom to see the vase
             Guest currentVaseVisitor = queueForVase.poll();
 
+            // If this is the current guest's first time visiting the vase, then increment the counter keeping track of how
+            // many guests have seen the vase by one
+            if (currentVaseVisitor.timesVisitedVase == 0) {
+                numGuestsVisitedVase.incrementAndGet();
+            }
+
             // Current guest in showroom prints what number visit they are making to see the vase if print flag is enabled
             if (PRINT_VASE_VISITORS) {
-                if (currentVaseVisitor.timesVisitedVase == 0) {
-                    System.out.println("Guest " + currentVaseVisitor.guestId + " is visiting the vase for the first time.");
-                }
-                else {
                     System.out.println("Guest " + currentVaseVisitor.guestId + " is making their visit number " + (currentVaseVisitor.timesVisitedVase + 1) + " to see the vase.");
-                }
             }
 
             // Guest is done visiting the vase and exits the showroom, so let the guest be free from the line until 
@@ -54,7 +61,7 @@ public class MinotaurCrystalVase {
         // Calculate Execution Time
         double elapsedSeconds = (endTime - startTime) / 1000.0;
 
-        System.out.println("All " + NUM_GUESTS + " guests got to see the vase as many times as they wanted in " + elapsedSeconds + " seconds.");
+        System.out.println("All " + NUM_GUESTS + " guests got to see the vase in " + elapsedSeconds + " seconds.");
     }
 }
 
@@ -78,20 +85,16 @@ class Guest extends Thread {
 
     @Override
     public void run() {
-        // Get a radom integer between 0 and 99 from thread-safe random number generator to represent their decision to
-        // initially join the queue to view the vase
-        int randInt = ThreadLocalRandom.current().nextInt(100);
+        // Guest should keep making decisions to join/rejoin the line while there are still some guests that have not seen the vase yet.
+        // Once all guests have seen the vase, then all guests can decide it's time to leave.
+        while (MinotaurCrystalVase.numGuestsVisitedVase.get() < MinotaurCrystalVase.NUM_GUESTS) {
+            // Get a radom integer between 0 and 99 from thread-safe random number generator to represent their decision to
+            // initially join the queue to view the vase
+            int randInt = ThreadLocalRandom.current().nextInt(100);
 
-        // If guest's random decision is within their percentage of willingless to join/rejoin the queue, then guest
-        // stays at the party and joins the queue
-        while (randInt < PERCENT_CHANCE_TO_QUEUE) {
-            // Have guest joing the back of the queue and make guest's state trasition to in line
-            inLine.set(true);
-            MinotaurCrystalVase.queueForVase.offer(this);
-
-            // Have guest keep waiting until they are finally able to leave the line
-            // and view the vase
-            while (inLine.get()) {
+            // If guest's random decision is not within their percentage of willingless to join/rejoin the queue, then guest
+            // takes a short break before trying again
+            if (randInt >= PERCENT_CHANCE_TO_QUEUE) {
                 try {
                     Thread.sleep(10);
                 }
@@ -99,13 +102,25 @@ class Guest extends Thread {
                     e.printStackTrace();
                 }
             }
+            else {
+                // Have guest join the back of the queue and make guest's state trasition to in line
+                inLine.set(true);
+                MinotaurCrystalVase.queueForVase.offer(this);
 
-            // Guest just left showroom and visited vase, so increment the current guest's number of vase visits by 1
-            this.timesVisitedVase++;
+                // Have guest keep waiting until they are finally able to leave the line
+                // and are done viewing the vase
+                while (inLine.get()) {
+                    try {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
 
-            // Now that guest is out of line, let guest make a random decision on whether to join the line to see the vase again.
-            // If not, the guest will decide to leave the party and the thread will stop executing for this guest.
-            randInt = ThreadLocalRandom.current().nextInt(100);
+                // Guest just left showroom and visited vase, so increment the current guest's number of vase visits by 1
+                this.timesVisitedVase++;
+            }
         }
     }
 }
